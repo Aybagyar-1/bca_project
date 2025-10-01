1b6ddf470561f973a010a6d9f56a5e2fc3402865
Aybagyar <banuislam4life@gmail.com>
2025-10-01 20:15:12 +0100

    Member class updated

--- src/main/java/model/Member.java
+++ src/main/java/model/Member.java
@@ -26,14 +26,24 @@ public class Member {
         this.status = status;
     }
 
-    // Getters and Setters
+    // Getters and setters
     public int getMemberId() { return memberId; }
+    public void setMemberId(int memberId) { this.memberId = memberId; }
     public String getSurname() { return surname; }
+    public void setSurname(String surname) { this.surname = surname; }
     public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
     public String getMiddleName() { return middleName; }
+    public void setMiddleName(String middleName) { this.middleName = middleName; }
     public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
     public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
     public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
     public Date getJoinDate() { return joinDate; }
+    public void setJoinDate(Date joinDate) { this.joinDate = joinDate; }
     public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
+
